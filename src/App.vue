9a38e4762956403f08846aea3c853ea2691c6895
Chandler Anderson <chandler.anderson@slalom.com>
2019-08-17 21:56:22 -0400

    Cleaned up old code.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <template>
   <div id="app">
     <div class="records">
-      <label for="releaseNo">Release To Get</label>
-      <input id="releaseNo" v-model.number="testRelease" />
-      <button v-on:click="fetchRecordData">Fetch</button>
+      <button v-on:click="fetchRecordData">GET BEAUTIFUL ARTWORK</button>
       <div>
-        <div v-for="track in tracklist">
-          {{ track.title }}
-        </div>
         <div v-for="record in records">
           <img v-bind:src="record.imageUrl">
           <div>
@@ -16,21 +11,6 @@
         </div>
       </div>
     </div>
-
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
