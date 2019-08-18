97146fe6eee3f06eaba8dd2726ecb08610e9d855
Chandler Anderson <chandler.anderson@slalom.com>
2019-08-18 15:13:46 -0400

    Clean up nav bar design.

--- src/components/RecordWall.vue
+++ src/components/RecordWall.vue
@@ -1,34 +1,38 @@
 <template>
   <div id="app">
-    <div>
-      <b-navbar fixed="top" type="light" variant="light">
-        <b-navbar-nav>
+    <b-navbar toggleable="lg" fixed="top" type="light" variant="light">
+      <b-navbar-brand href="#">My Record Wall</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
           <b-nav-form>
             <b-form-input v-model.trim="searchInput" class="mr-sm-2" placeholder="Search Artist/Album..."></b-form-input>
           </b-nav-form>
-        </b-navbar-nav>
 
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-form-group>
-            <b-form-radio-group
-              id="btn-radios-1"
-              v-model="sortDirection"
-              :options="sortDirectionOptions"
-              buttons
-              name="radios-btn-default">
-            </b-form-radio-group>
-            <b-form-radio-group
-              id="btn-radios-2"
-              v-model="sortCriteria"
-              :options="sortCriteriaOptions"
-              buttons
-              name="radios-btn-default">
-            </b-form-radio-group>
-          </b-form-group>
+          <b-nav-form>
+            <b-form-group>
+              <b-form-radio-group
+                id="btn-radios-1"
+                v-model="sortDirection"
+                :options="sortDirectionOptions"
+                buttons
+                name="radios-btn-default">
+              </b-form-radio-group>
+              <b-form-radio-group
+                id="btn-radios-2"
+                v-model="sortCriteria"
+                :options="sortCriteriaOptions"
+                buttons
+                name="radios-btn-default">
+              </b-form-radio-group>
+            </b-form-group>
+          </b-nav-form>
         </b-navbar-nav>
-      </b-navbar>
-    </div>
+      </b-collapse>
+    </b-navbar>
 
     <div class="records" v-if="allRecords.length > 0">
       <b-container fluid class="wide-container">
