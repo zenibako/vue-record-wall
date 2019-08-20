94d4a2b7560b07cd0f9a396cc26c2dae185e712e
Chandler Anderson <chandler.anderson@slalom.com>
2019-08-19 22:51:14 -0400

    Add Heroku config vars.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,7 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  DISCOGS_TOKEN: JSON.stringify(process.env.DISCOGS_TOKEN),
+  SPOTIFY_CLIENT_KEY: JSON.stringify(process.env.SPOTIFY_CLIENT_KEY),
+  SPOTIFY_CLIENT_SECRET: JSON.stringify(process.env.SPOTIFY_CLIENT_SECRET),
 }
